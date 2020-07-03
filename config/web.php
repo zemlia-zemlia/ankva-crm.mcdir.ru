8d87576c5d24e5c7998d9dd9d44e34c66fe90e09
andrey <andrey@spb-abris.ru>
2020-07-03 16:20:08 +0300

    ip

--- config/web.php
+++ config/web.php
@@ -218,14 +218,14 @@ if (YII_ENV_DEV) {
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
-       'allowedIPs' => ['127.0.0.1',  '79.126.56.121','::1'],
+       'allowedIPs' => ['*',  '79.126.56.121','::1'],
     ];
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
-       'allowedIPs' => ['*', '79.126.56.121', '::1'],
+       'allowedIPs' => ['127.0.0.1', '79.126.56.121', '::1'],
     ];
 }
 
